8a09d8acfcbb2486b2eb7b04552a996f044d858d
seohyeon <s24005@gsm.hs.kr>
2024-12-15 17:58:08 +0900

    Nav var

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import Main from "./pages/Main";
+import Nav from "./pages/Nav";
 
 const HomePage: React.FC = () => {
-  return <Main />;
+  return <Nav />;
 };
 
 export default HomePage;
