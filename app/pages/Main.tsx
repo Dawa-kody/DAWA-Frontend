8130095e24bddc43ca0c403c00678c2f94d6e477
jgh진건희 <s23054@gsm.hs.kr>
2024-12-16 15:46:28 +0900

    메인 비활성화(Nav 없는 버전, 기능 X) 만듬

--- app/pages/Main.tsx
+++ app/pages/Main.tsx
@@ -25,6 +25,32 @@ function Main(){
                     </S.TeacherIconDiv>
                     <S.TeacherState>선생님 부재중</S.TeacherState>
                 </S.TeacherSection>
+
+                <S.VisitModalDiv>
+                    <S.ModalTitle>방문 기록 작성</S.ModalTitle>
+                    <S.ModalButton>방문기록 작성</S.ModalButton>
+                </S.VisitModalDiv>
+
+                <S.RentDiv>
+                    <S.RentTitle>대여기록</S.RentTitle>
+                    <S.RentNonActiveSpan>대여한 기록이 존재하지 않습니다.</S.RentNonActiveSpan>
+                </S.RentDiv>
+
+                <S.VisitDiv>
+                    <S.VisitTitle>방문기록</S.VisitTitle>
+                    <S.VisitNonActiveSpan>방문한 기록이 존재하지 않습니다.</S.VisitNonActiveSpan>
+                </S.VisitDiv>
+
+                <S.BedDiv>
+                    <S.BedTitle>침대 현황</S.BedTitle>
+                    <S.BedMenNonActiveDiv>
+                        <S.BedIcon src={"/Bed.svg"} />
+                    </S.BedMenNonActiveDiv>
+
+                    <S.BedWomenNonActiveDiv>
+                        <S.BedIcon src={"/Bed.svg"} />
+                    </S.BedWomenNonActiveDiv>
+                </S.BedDiv>
             </S.Container>
         </>
     );
