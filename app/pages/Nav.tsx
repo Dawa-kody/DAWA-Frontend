ed17013b11cc766b1ee99537910aa89401aa4ee5
seohyeon <s24005@gsm.hs.kr>
2024-12-18 09:29:34 +0900

    Nav바 만들기 4일차

--- app/pages/Nav.tsx
+++ app/pages/Nav.tsx
@@ -1,43 +1,53 @@
-'use client';
-import React from 'react'
-import { useRouter } from 'next/router';
-import * as S from'../styles/Nav';
-function Nav() {
-  return (
- <>
+  'use client';
 
-<S.Component>
-<div>
-<S.LogoText>대시보드</S.LogoText>
- <S.Logo src={'./Logo.png'} alt='로고' ></S.Logo>
-</div>
+  import React, { useState } from 'react';
+  import { useRouter } from 'next/router';
+  import * as S from '../styles/Nav';
 
-<div>
-<S.HomeText>응급처치</S.HomeText>
- <S.Home src={'./HomeWhite.png '} alt='홈'></S.Home>
-</div>
-
-<div>
-<S.DangerousText>프로필</S.DangerousText>
- <S.Dangerous src={'./ActivityPurple.png'} alt='응급사항'></S.Dangerous>
-</div>
+  function Nav() {
+    const [Homeactive, HomesetActive] = useState<boolean>(false);
+    const [Dangerousactive, DangeroussetActive] = useState<boolean>(false);
 
 
- <S.Login src={'./PersonPurple.png '} alt='프로필'></S.Login>
+  const handleHome = () => {
+    HomesetActive(!Homeactive);``
+  };
+  
+  const handleDangerous = () => {
+    DangeroussetActive(!Dangerousactive);
+  };
+  ``
+      
+    return (
+      <>
+        <S.Component>
+          <S.LogoHap>
+            <S.LogoText>다와</S.LogoText>
+            <S.Logo src={'./Logo.png'} alt="로고" />``
+          </S.LogoHap>
 
+          <S.HomeHap onClick={handleHome}>
+            <S.HomeText active={Homeactive}>대시보드</S.HomeText>
+            <S.Home
+              src={Homeactive ? './HomePurplr.png' : './HomeWhite.png'}
+              alt="홈"
+            />
+          </S.HomeHap>
 
+          <S.DangerousHap onClick={handleDangerous}>
+            <S.DangerousText active={Dangerousactive}>응급처치</S.DangerousText>
+            <S.Dangerous
+              src={Dangerousactive ? './ActivityPurple.png' : './UnActivityWhit.png'}
+              alt="응급사항"
+            />
+          </S.DangerousHap>
 
+          <S.Login src={'./PersonPurple.png'} alt="프로필" />
+        </S.Component>
 
-</S.Component>
+        <S.Momoka src={'./mokoko.png'} />
+      </>
+    );
+  }
 
-<S.Momoka src={'./mokoko.png'} />
- </>
-  )
-}
-
-export default Nav
-
-
-
-
-
+  export default Nav;
--- app/styles/Nav.ts
+++ app/styles/Nav.ts
@@ -1,32 +1,44 @@
 import styled from "styled-components";
 
-export const Logo=styled.img`
 
+export const LogoHap=styled.div`
+
+`
+export const Logo=styled.img`
 position: absolute;
+width:56px;
+height:42px;
+
 height: 44.87px;
-left:130px;
-top:20px;
+top:8px;
+left:100px;
+
 `;
 
 export const LogoText=styled.span`
-
-font-family: 'Pretendard Variable';
+font-family: 'Pretendard';
 font-style: normal;
 font-weight: 700;
-font-size: 20px;
-line-height: 24px;
+font-size: 30px;
+line-height: 36px;
 
-color: #DEDEDE;
+color: #6938EF;
+position:relative;
+top:20px;
+left:180px;
+
 `
 
 
 export const Component=styled.div`
-
-width: 1520px;
-height: 100px;
-
+width: 100%;
+height: 70px;
 background: #FFFF;
 z-index:-1;
+-webkit-user-select:none;
+-moz-user-select:none;
+-ms-user-select:none;
+user-select:none
 `; 
 export const Momoka=styled.img`
 width: 1000px;
@@ -34,34 +46,35 @@ height: 1000px;
 
 `
 
-
+  export const HomeHap=styled.div`
+  cursor: pointer;
+    display: flex;
+  align-items: center;
+  `
 
 export const Home=styled.img`
 position: absolute;
-left: 1000px;
-top:30px;
+left: 945px;
+top:20px;
 `
 
-export const HomeText=styled.div`
+export const HomeText=styled.span<{ active: boolean }>`
+color: ${(props) => (props.active ? '#6948ED' : '#DEDEDE')};
 font-family: 'Pretendard Variable';
 font-style: normal;
 font-weight: 700;
 font-size: 20px;
 line-height: 24px;
-
-color: #DEDEDE;
-
-margin-left: 1060px;
-margin-top:-100px;
+position:absolute;
+left:1000px;
+top:25px;
 `
-
-
 
 
 export const Login=styled.img`
 position: absolute;
-left: 1400px;
-top:30px;
+left: 1350px;
+top:20px;
 `
 
 export const LoginText=styled.span`
@@ -76,19 +89,24 @@ color: #DEDEDE;
 `
 
   
+export const DangerousHap=styled.div`
+cursor: pointer;
+`
 
 export const Dangerous=styled.img`
 position: absolute;
-left: 1200px;
-top:30px;
+left: 1150px;
+top:20px;
 `
-export const DangerousText=styled.span`
+export const DangerousText=styled.span<{ active: boolean }>`
+color: ${(props) => (props.active ? '#6948ED' : '#DEDEDE')};
 font-family: 'Pretendard Variable';
 font-style: normal;
 font-weight: 700;
 font-size: 20px;
 line-height: 24px;
-
-color: #DEDEDE;
+position:absolute;
+left:1200px;
+top:23px;
 
 `
