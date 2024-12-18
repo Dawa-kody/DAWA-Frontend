bc098f6c03705f972e40fc3287ce546d0ff738ea
jgh진건희 <s23054@gsm.hs.kr>
2024-12-18 20:38:11 +0900

    FilterTag 박스의 태그 컴포넌트 토글 버튼 기능 만듬(테스트 X)

--- app/components/FilterTag.tsx
+++ app/components/FilterTag.tsx
@@ -1,38 +1,43 @@
 import React, { useState, useEffect } from "react";
-import * as S from "../styles/FirstAid";
+import * as S from "../styles/FilterTag";
 import axios from "axios";
 
 interface unHealth {
     Name: string;
 }
 
-function FilterTag(){
+function FilterTag() {
     const [Disease, SetDisease] = useState<unHealth[]>([]);
+    const [Active, setActive] = useState(false);
 
     useEffect(() => {
-            async function fetchDiseaseData() {
-                try {
-                    const response = await axios.get(`${process.env.NEXT_PUBLIC_REACT_APP_BASE_URL}/무언가`);
-                    SetDisease(response.data);
-                } catch (error) {
-                    console.error("병명 데이터를 불러오는 중 에러 발생:", error);
-                }
+        async function fetchDiseaseData() {
+            try {
+                const response = await axios.get(`${process.env.NEXT_PUBLIC_REACT_APP_BASE_URL}/무언가`);
+                SetDisease(response.data);
+            } catch (error) {
+                console.error("병명 데이터를 불러오는 중 에러 발생:", error);
             }
-            fetchDiseaseData();
-        }, []);
+        }
+        fetchDiseaseData();
+    }, []);
 
-    return(
+    return (
         <>
-            <S.KeywordTitle>증상 키워드를 골라보세요</S.KeywordTitle>
-                <S.Line />
-                <S.FilterTagBox>
-                    {Disease.length > 0 ? (
-                        Disease.map((disease, index) => (
-                            <S.FilterTag key={index}>{`#${disease.Name}`}</S.FilterTag>
-                        ))
-                    ) : (
-                        <div />
-                    )}
+            <S.FilterTagBox Active={Active}>
+                {Disease.length > 0 ? (
+                    Disease.map((disease, index) => (
+                        <S.FilterTag key={index}>{`#${disease.Name}`}</S.FilterTag>
+                    ))
+                ) : (
+                    <div />
+                )}
+
+                <S.TrinangleButton
+                    src={"/TriangleButton.svg"}
+                    Active={Active}
+                    onClick={() => setActive(!Active)}
+                />
             </S.FilterTagBox>
         </>
     );
