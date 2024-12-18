199383c9c2b4f0699753856a68b5d9c36ee62427
jgh진건희 <s23054@gsm.hs.kr>
2024-12-18 10:07:55 +0900

    Nav 관련 사소한 문제 될만한 것들 미리 배제

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ const HomePage: React.FC = () => {
   return(
     <>
       <Nav />
+      {/* <Main /> */}
     </>
   );
 };
--- app/pages/Nav.tsx
+++ app/pages/Nav.tsx
@@ -10,26 +10,26 @@
 
 
   const handleHome = () => {
-    HomesetActive(!Homeactive);``
+    HomesetActive(!Homeactive);
   };
   
   const handleDangerous = () => {
     DangeroussetActive(!Dangerousactive);
   };
-  ``
+  
       
     return (
       <>
         <S.Component>
           <S.LogoHap>
             <S.LogoText>다와</S.LogoText>
-            <S.Logo src={'./Logo.png'} alt="로고" />``
+            <S.Logo src={'Logo.png'} alt="로고" />
           </S.LogoHap>
 
           <S.HomeHap onClick={handleHome}>
             <S.HomeText active={Homeactive}>대시보드</S.HomeText>
             <S.Home
-              src={Homeactive ? './HomePurplr.png' : './HomeWhite.png'}
+              src={Homeactive ? 'HomePurplr.png' : 'HomeWhite.png'}
               alt="홈"
             />
           </S.HomeHap>
@@ -37,15 +37,13 @@
           <S.DangerousHap onClick={handleDangerous}>
             <S.DangerousText active={Dangerousactive}>응급처치</S.DangerousText>
             <S.Dangerous
-              src={Dangerousactive ? './ActivityPurple.png' : './UnActivityWhit.png'}
+              src={Dangerousactive ? 'ActivityPurple.png' : 'UnActivityWhit.png'}
               alt="응급사항"
             />
           </S.DangerousHap>
 
           <S.Login src={'./PersonPurple.png'} alt="프로필" />
         </S.Component>
-
-        <S.Momoka src={'./mokoko.png'} />
       </>
     );
   }
