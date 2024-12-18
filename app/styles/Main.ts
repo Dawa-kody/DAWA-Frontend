481cbc9873209d45cff3d316ed0fb7fbfa68cf14
jgh진건희 <s23054@gsm.hs.kr>
2024-12-18 19:17:03 +0900

    배경 white -> 회색으로 변경 로그인, 회원가입, 비밀번호 변경 페이지는 해당 안됨

--- app/styles/Main.ts
+++ app/styles/Main.ts
@@ -3,9 +3,8 @@ import styled from "styled-components";
 export const Container = styled.div`
     width: 100vw;
     height: 100vh;
+    
     display: flex;
-
-    background-color: #F2F4F7;
 `
 
 /* 선생님 현황 */
