91fcaaaeb67d282932f19a2452d7e93ca38fbf89
박서현 <s24005@gsm.hs.kr>
2024-12-23 23:52:47 +0900

    프로필 호버했을떄

--- app/pages/Nav.tsx
+++ app/pages/Nav.tsx
@@ -5,32 +5,45 @@ import * as S from '../styles/Nav';
 
 function Nav() {
   const [activeMenu, setActiveMenu] = useState<'home' | 'dangerous' | null>(null);
+  const [MouseOver, setMouseOver] = useState(false); 
 
   const handleClick = (menuName: 'home' | 'dangerous') => {
     setActiveMenu(menuName === activeMenu ? null : menuName);
   };
 
+
   return (
     <>
       <S.Component>
-        <S.LogoHap>
-          <S.LogoText>다와</S.LogoText>
-          <S.Logo src={'Logo.png'} alt="로고" />
-        </S.LogoHap>
-        <S.HomeHap onClick={() => handleClick('home')} $active={activeMenu === 'home'}>
-  <S.Home src={activeMenu === 'home' ? 'HomePurple.png' : 'HomeWhite.png'} alt="홈" />
-  <S.HomeText active={activeMenu === 'home'}>대시보드</S.HomeText>
+        
+         <S.LogoHap>
+             <S.LogoText>다와</S.LogoText>
+            <S.Logo src={'Logo.png'} alt="로고" />
+         </S.LogoHap>
+ 
+         <S.HomeHap onClick={() => handleClick('home')} $active={activeMenu === 'home'}>
+            <S.Home src={activeMenu === 'home' ? 'HomePurple.png' : 'HomeWhite.png'} alt="홈" />
+            <S.HomeText active={activeMenu === 'home'}>대시보드</S.HomeText>
+         </S.HomeHap>
+ 
+         <S.DangerousHap onClick={() => handleClick('dangerous')} $active={activeMenu === 'dangerous'}>
+            <S.Dangerous
+              src={activeMenu === 'dangerous' ? 'ActivityPurple.png' : 'UnActivityWhite.png'} alt="응급사항" />
+            <S.DangerousText active={activeMenu === 'dangerous'}>응급처치</S.DangerousText>
+         </S.DangerousHap>
+ 
+         <S.Login onMouseLeave={()=>setMouseOver(false) } onMouseEnter={()=>setMouseOver(true)} src={'./PersonPurple.png'}alt="프로필"/>
+         {MouseOver && (
+        <S.LoginHoverBox onMouseLeave={() => setMouseOver(false)} 
+                         onMouseEnter={() => setMouseOver(true)}>
+            <S.TextContainer>
+                <S.Text>로그아웃</S.Text>
+                <S.Text>비밀번호 변경</S.Text>
+            </S.TextContainer>
+        </S.LoginHoverBox>
+  )}
+</S.Component>
 
-</S.HomeHap>
-
-<S.DangerousHap onClick={() => handleClick('dangerous')} $active={activeMenu === 'dangerous'}>
-  <S.Dangerous src={activeMenu === 'dangerous' ? 'ActivityPurple.png' : 'UnActivityWhite.png'} alt="응급사항" />
-  <S.DangerousText active={activeMenu === 'dangerous'}>응급처치</S.DangerousText>
-</S.DangerousHap>
-
-
-        <S.Login src={'./PersonPurple.png'} alt="프로필" />
-      </S.Component>
     </>
   );
 }
