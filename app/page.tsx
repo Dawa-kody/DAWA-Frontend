844dbdd640cf35050e770fb44042c7249f701c40
jgh진건희 <s23054@gsm.hs.kr>
2025-02-15 14:22:44 +0900

    app/page.tsx 경로 Main으로 변경

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,10 @@
 import React from "react";
 import Main from "./pages/Main";
-import Nav from "./components/Nav";
-import Sheet from "./pages/sheet"
-import FirstAid from "./pages/FirstAid";
-
 
 const HomePage: React.FC = () => {
   return(
     <>
-      {/* <Nav />   */}
-      {/*<Main />*/}
-      <Sheet/>
+      <Main />
     </>
   );
 };
