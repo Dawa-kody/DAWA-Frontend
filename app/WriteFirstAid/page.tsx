db0b2dd605d3d5a1631fb9a9da3aee108291863a
박서현 <s24005@gsm.hs.kr>
2025-02-18 23:35:55 +0900

    FirstAidAdmin 관련 css 수정

---
+++ app/WriteFirstAid/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+import React from 'react';
+import WriteFirstAid from '../pages/WriteFistAid';
+
+const WriteFirstAidPage: React.FC = () => {
+  return <WriteFirstAid/>;
+};
+
+export default WriteFirstAidPage;
