c8fd70031a093d2e8bfbf44ca11e132042a56aea
tlrdmsEjrqhRdl <s24030@gsm.hs.kr>
2025-03-17 19:47:33 +0900

    a

--- app/pages/Main.tsx
+++ app/pages/Main.tsx
@@ -180,18 +180,6 @@ function Main() {
           </S.VisitDataCards>
         </S.VisitDiv>
 
-        <S.BedDiv>
-          <S.BedTitle>침대 사용 여부</S.BedTitle>
-          <S.BedMenNonActiveDiv Active={bedStatus.bed1}>
-            <S.BedIcon src={"/Bed.svg"} />
-            {bedStatus.bed1 ? <S.BedIsFree>침대 사용 가능</S.BedIsFree> : <S.BedIsFree>침대 사용 중</S.BedIsFree>}
-          </S.BedMenNonActiveDiv>
-
-          <S.BedWomenNonActiveDiv Active={bedStatus.bed2}>
-            <S.BedIcon src={"/Bed.svg"} />
-            {bedStatus.bed2 ? <S.BedIsFree>침대 사용 가능</S.BedIsFree> : <S.BedIsFree>침대 사용 중</S.BedIsFree>}
-          </S.BedWomenNonActiveDiv>
-        </S.BedDiv>
 
       </S.Container>
     </>
