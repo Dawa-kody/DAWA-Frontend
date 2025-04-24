da20a444950e504755c10caa3044bcc9a8393715
jgh진건희 <s23054@gsm.hs.kr>
2025-04-24 18:07:10 +0900

    bugfix: 선생님 메인 페이지에 대여 신청 request 관련 컴포넌트가 없었음

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import scrollbarHide from 'tailwind-scrollbar-hide'
 
 const config: Config = {
   content: [
@@ -14,8 +15,22 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      keyframes: {
+        slideIn: {
+          from: { transform: 'translateX(100%)' },
+          to: { transform: 'translateX(0)' },
+        },
+        slideOut: {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(100%)' },
+        },
+      },
+      animation: {
+        slideIn: 'slideIn 0.4s ease-in-out',
+        slideOut: 'slideOut 0.4s ease-in-out forwards',
+      },
     },
   },
-  plugins: [],
+  plugins: [scrollbarHide],
 };
 export default config;
