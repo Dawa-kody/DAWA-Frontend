ded2a703b6f96096bd17c896c59f9cdac649eb1e
tlrdmsEjrqhRdl <s24030@gsm.hs.kr>
2025-04-29 16:57:43 +0900

    메인 공지사항

---
+++ app/organisms/NoticeModal.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function NoticeModal () {
+    
+  return (
+    <div>
+      <h1>Notice Modal</h1>
+      {/* Add your modal content here */}
+    </div>
+  );
+}
+
+export default NoticeModal;
