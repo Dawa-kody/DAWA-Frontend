d618667228773e9c8435a7da93acb78b9946b0a4
jgh진건희 <s23054@gsm.hs.kr>
2025-05-27 09:07:51 +0900

    bugfix: method

--- app/API/dust.ts
+++ app/API/dust.ts
@@ -4,7 +4,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const { sidoName } = req.query;
     const apiKey = process.env.NEXT_PUBLIC_DAWA_WEATHER_API_KEY;
 
-    const url = `http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?serviceKey=${apiKey}&sidoName=${sidoName}&pageNo=1&numOfRows=100&dataTerm=DAILY&ver=1.3&returnType=JSON`;
+    const url = `https://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?serviceKey=${apiKey}&sidoName=${sidoName}&pageNo=1&numOfRows=100&dataTerm=DAILY&ver=1.3&returnType=JSON`;
 
     try {
         const response = await fetch(url);
